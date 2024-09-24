0fb7db401137ae16a486f5d0b564a5610b537a9a
Voloschuk08 <voloschuk07olia@gmail.com>
2024-09-24 21:46:22 +0300

    Add files via upload

---
+++ PR3.4/PR3.4/PR3.4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main()
+{
+	double x;// вхідний аргумент 
+	double y;// вхідний параметр 
+	double R;// радіус 
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+	// розгалуження в повній формі 
+	if ((x*x+y*y<=R*R && y>0 && y>x) ||
+		(x * x + y * y <= R * R && y<0 && x>y))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
